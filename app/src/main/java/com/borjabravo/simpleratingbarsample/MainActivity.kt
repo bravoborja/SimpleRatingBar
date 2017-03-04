6854f745905d0c45c9665272098a367570a7c926
Borja Bravo <borja.bravoalvarez@gmail.com>
2017-03-04 23:29:25 +0100

    Change sample app

--- app/src/main/java/com/borjabravo/simpleratingbarsample/MainActivity.kt
+++ app/src/main/java/com/borjabravo/simpleratingbarsample/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.borjabravo.simpleratingbarsample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import com.borjabravo.simpleratingbar.SimpleRatingBar
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnChangeIndicator = findViewById(R.id.btn_change_indicator) as Button
+        val btnChangeRating = findViewById(R.id.btn_change_rating) as Button
+        val btnChangeRatingMargin = findViewById(R.id.btn_change_rating_margin) as Button
+        val simpleRatingBar = findViewById(R.id.simple_rating_bar) as SimpleRatingBar
+
+        btnChangeIndicator.setOnClickListener({ simpleRatingBar.isIndicator = !simpleRatingBar.isIndicator })
+        btnChangeRating.setOnClickListener({
+            val random = Random()
+            val rating = random.nextFloat() * 6f
+            simpleRatingBar.rating = rating
+        })
+        btnChangeRatingMargin.setOnClickListener({
+            val random = Random()
+            val ratingMargin = random.nextInt(80) + 5
+            simpleRatingBar.ratingMargin = ratingMargin
+        })
     }
 }
